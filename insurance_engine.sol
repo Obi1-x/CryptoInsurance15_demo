35e764688d02b52309a2d33224762b7bc2cfdc1f
Okezie <chekwube102@gmail.com>
2021-04-06 02:01:26 +0100

    Genesis

---
+++ insurance_engine.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.8.0;
+// Imports token contracts from finance department.
+//import "./Hashsurance.sol"; //@ phillip, kindly insert the tokens you created to this statement
+import "./policy_contract.sol";
+
+contract Insurengine{
+  Policy policies;
+
+}
---
+++ policy_contract.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.8.0;
+
+contract Policy{
+  address policyHolder;
+  uint principal;
+
+  constructor(address owner){
+    policyHolder = owner;
+  }
+}
