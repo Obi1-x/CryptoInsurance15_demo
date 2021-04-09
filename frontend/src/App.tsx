7340600fc8c8acee22be9fcd3bddb27f6a3e826c
Alabi Temitope <t.banji@rocketmail.com>
2021-04-09 14:30:20 +0100

    updated with hashsurance method in the frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,82 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import getBlockchain from './ethereum.js';
+import env from './env';
 import './App.css';
 
+// interface inputDataMsg {
+//   username: string;
+//   prevState: undefined
+// }
+
 function App() {
+  const [hashsurance, setHashsurance] = useState(undefined);
+  const [data, setData] = useState<string | undefined>(undefined);
+
+  useEffect(() => {
+    console.log('env', env.api);
+    const init = async () => {
+      const { hashsurance } = await getBlockchain();
+      if ('readData()' in hashsurance === false) {
+        setHashsurance(hashsurance);
+        setData('Select Required Metamask network');
+        return;
+      }
+      const data = await hashsurance.readData();
+      setHashsurance(hashsurance);
+      setData(data);
+    };
+    init();
+  }, []);
+
+  const updateData = async e => {
+    e.preventDefault();
+    const data = e.target.elements[0].value;
+    const tx = await hashsurance.updateData(data);
+    await tx.wait();
+    const newData = await hashsurance.readData();
+    setData(newData);
+  };
+
+
+  if (
+    (typeof helloWorld === 'undefined'
+      || typeof data === 'undefined')
+  ) {
+    console.log(helloWorld, data);
+    return `<div>Loading...</div>`;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      {/* if Metamask not selected prompt message */}
+      {'readData()' in hashsurance === false && <div className="row">{data.toString()}</div>}
+
+      {/* if Metamask is selectected */}
+      {'readData()' in hashsurance === true && <div className='row'>
+        <div className='col-sm-6'>
+          <h2>Data:</h2>
+          <p>{data.toString()}</p>
+        </div>
+
+        <div className='col-sm-6'>
+          <h2>Change data</h2>
+          <form className="form-inline" onSubmit={e => updateData(e)}>
+            <input
+              type="text"
+              className="form-control"
+              placeholder="data"
+            />
+            <button
+              type="submit"
+              className="btn btn-primary"
+            >
+              Submit
+    </button>
+          </form>
+        </div>
+
+      </div>}
+
     </div>
   );
 }
---
+++ frontend/src/ethereum.js
@@ -0,0 +1,36 @@
+// use to interact to Metamask which we will use to manage ethereum
+import detectEthereumProvider from '@metamask/detect-provider';
+import { ethers, Contract } from 'ethers';
+// import SimpleStorage from './contracts/SimpleStorage.json';
+import Hashsurance from './contracts/Hashsurance.json';
+
+const getBlockchain = () =>
+    new Promise(async (resolve, reject) => {
+        let provider = await detectEthereumProvider();
+        if (provider) {
+            console.log('provider', provider);
+            await provider.request({ method: 'eth_requestAccounts' });
+            const networkId = await provider.request({ method: 'net_version' })
+            provider = new ethers.providers.Web3Provider(provider);
+            const signer = provider.getSigner();
+            if (Hashsurance.networks[networkId]) {
+                const hashsurance = new Contract(
+                    Hashsurance.networks[networkId].address,
+                    Hashsurance.abi,
+                    signer // this will help us send transaction thereby making our communication to be secure
+                );
+                console.log('{ hashsurance }', { hashsurance });
+                resolve({ hashsurance });
+                return;
+            } else {
+                const helloWorld = { data: 'Select Required Metamask network' };
+                console.log('{ helloWorld }', { helloWorld });
+                resolve({ helloWorld });
+                return;
+            }
+
+        }
+        reject('Install Metamask');
+    });
+
+export default getBlockchain;
